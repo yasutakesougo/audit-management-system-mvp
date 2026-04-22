8326eb60a61933dff662c64f081d895f707b5d19
yasutakesougo <momosantanuki@gmail.com>
2026-04-22 09:14:37 +0900

    fix(schedules): restore missing sharePointAdapter compatibility export

--- src/features/schedules/data/index.ts
+++ src/features/schedules/data/index.ts
@@ -1,9 +1,11 @@
 export { SchedulesProvider, useSchedulesPort } from './context';
-export { normalizeUserId } from './createAdapters';
+export { makeMockScheduleCreator, makeSharePointScheduleCreator, normalizeUserId } from './createAdapters';
 export { demoSchedulesPort } from './demoAdapter';
 export { makeGraphSchedulesPort } from './graphAdapter';
 export type { InlineScheduleDraft } from './inlineScheduleDraft';
 export type {
-	CreateScheduleEventInput, DateRange, SchedItem, ScheduleCategory,
-	ScheduleServiceType, SchedulesPort, ScheduleStatus, ScheduleVisibility, UpdateScheduleEventInput
+    CreateScheduleEventInput, DateRange, SchedItem, ScheduleCategory,
+    ScheduleServiceType, SchedulesPort, ScheduleStatus, ScheduleVisibility, UpdateScheduleEventInput
 } from './port';
+export { getListFieldsMeta, makeSharePointSchedulesPort } from './sharePointAdapter';
+export type { ListFieldMeta } from './sharePointAdapter';
